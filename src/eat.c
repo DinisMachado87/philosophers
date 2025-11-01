3c044ce87f9c4196b68452558f083bbde4a15779
dimachad <dinisma@gmail.com>
2025-11-01 04:34:39 +0100

    Move reseting next death time for beginning of eating rather than end

--- src/eat.c
+++ src/eat.c
@@ -6,7 +6,7 @@
 /*   By: dimachad <dimachad@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 15:25:40 by dimachad          #+#    #+#             */
-/*   Updated: 2025/11/01 03:31:34 by dimachad         ###   ########.fr       */
+/*   Updated: 2025/11/01 04:30:27 by dimachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,13 @@ static inline int	unlock_fork(
 	return (OK);
 }
 
-static inline void	safe_update_last_meal(t_philo *ph, t_state *s)
+static inline int	safe_update_last_meal(t_philo *ph, t_state *s)
 {
 	mtx_lock_tracked(&ph->mtx_philo, s);
 	ph->nxt_death = now(&ph->time, s) + s->t_die;
 	ph->n_eats++;
 	mtx_unlock_tracked(&ph->mtx_philo, s);
+	return (is_end(s));
 }
 
 int	eat(t_philo *ph, t_state *s)
@@ -52,9 +53,9 @@ int	eat(t_philo *ph, t_state *s)
 	if (OK == lock_fork(ph->fork_1, &locked_1, ph, s)
 		&& ph->fork_1 != ph->fork_2
 		&& OK == lock_fork(ph->fork_2, &locked_2, ph, s)
-		&& OK == safe_print("is eating\n", ph, s)
-		&& OK == wait_and_watch(s->t_eat, s, &ph->time))
-		safe_update_last_meal(ph, s);
+		&& OK == safe_update_last_meal(ph, s)
+		&& OK == safe_print("is eating\n", ph, s))
+		wait_and_watch(s->t_eat, s, &ph->time);
 	unlock_fork(ph->fork_1, &locked_1, s);
 	unlock_fork(ph->fork_2, &locked_2, s);
 	return (is_end(s));
